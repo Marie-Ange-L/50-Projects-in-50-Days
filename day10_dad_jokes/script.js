ec2afc7f8a2d6a079f7b578f3a06a84609fae36d
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-10-13 10:56:28 +0200

    Day 10 - Dad Jokes

---
+++ day10_dad_jokes/script.js
@@ -0,0 +1,31 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
+
+// Using ASYNC/AWAIT
+async function generateJoke() {
+	const config = {
+		headers: { Accept: "application/json" },
+	};
+
+	const response = await fetch("https://icanhazdadjoke.com", config);
+
+	const data = await response.json();
+
+	jokeEl.innerHTML = data.joke;
+}
+
+// Using THEN
+// function generateJoke() {
+// 	const config = {
+// 		headers: { Accept: "application/json" },
+// 	};
+// 	fetch("https://icanhazdadjoke.com", config)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			jokeEl.innerHTML = data.joke;
+// 		});
+// }
