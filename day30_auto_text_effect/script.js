8755b523064c29a4ce144e5146169bca6e1cd24c
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-11-06 10:55:17 +0100

    Day 30 - Auto Text Effect

---
+++ day30_auto_text_effect/script.js
@@ -0,0 +1,24 @@
+const textEl = document.getElementById("text");
+const speedEl = document.getElementById("speed");
+const text = "We Love Programming!";
+let index = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+	textEl.innerText = text.slice(0, index);
+
+	index++;
+
+	if (index > text.length) {
+		index = 1;
+	}
+
+	setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener(
+	"input",
+	(event) => (speed = 300 / event.target.value)
+);
