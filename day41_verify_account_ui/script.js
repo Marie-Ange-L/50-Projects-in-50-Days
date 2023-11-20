13f7e4492a76507836aae7ce285379b73868845f
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2023-11-20 12:42:01 +0100

    Day 41 - Verify Account UI

---
+++ day41_verify_account_ui/script.js
@@ -0,0 +1,15 @@
+const codes = document.querySelectorAll(".code");
+
+// codes[0].focus();
+// désactivé à cause de 'lintégration dans la page réunissant tous les projets
+
+codes.forEach((code, index) => {
+	code.addEventListener("keydown", (event) => {
+		if (event.key >= 0 && event.key <= 9) {
+			codes[index].value = "";
+			setTimeout(() => codes[index + 1].focus(), 10);
+		} else if (event.key === "Backspace") {
+			setTimeout(() => codes[index - 1].focus(), 10);
+		}
+	});
+});
